5f9120d510086fed5ae107857d9cba75c564394c
Marc Capdevielle <marc.capdevielle@orange.com>
2016-04-06 18:39:16 +0200

    js: reverse logging order

--- static/script.js
+++ static/script.js
@@ -7,46 +7,47 @@ var buttonBlink = $('light-blink');
 var contentMessage = $('echo-content');
 
 var WebsocketClass = function(host){
-   this.socket = new WebSocket(host);
-   this.console = $('console');
+    this.socket = new WebSocket(host);
+    this.console = $('console');
 };
 
 WebsocketClass.prototype = {
        initWebsocket : function(){
-	       var $this = this;
-	       this.socket.onopen = function(){
-		       $this.log('socket opened:'+ this.readyState);
+           var $this = this;
+           this.socket.onopen = function(){
+               $this.log('socket opened:'+ this.readyState);
                buttonOff.disabled = false;
                buttonOn.disabled = false;
                buttonBlink.disabled = false;
-	       };
-	       this.socket.onmessage = function(e){
+           };
+           this.socket.onmessage = function(e){
               $this.log('message received');
               var e = JSON.parse(e.data);
               var time = new Date();
               var message = time.getHours() + ":" + time.getMinutes() + ":" + time.getSeconds() + ": ";
               message += 'Light: ' + e.lightOn + ' Luminosity: ' + e.luminosity + '<br />';
-              contentMessage.innerHTML += message
-    	      contentMessage.scrollTop = contentMessage.scrollHeight;
-	       };
-	       this.socket.onclose = function(){
+              contentMessage.innerHTML = message + contentMessage.innerHTML;
+              contentMessage.scrollTop = 0;
+          };
+          this.socket.onclose = function(){
                $this.log('socket closed');
                buttonOff.disabled = true;
                buttonOn.disabled = true;
                buttonBlink.disabled = true;
-	       };
-	       this.socket.onerror = function(error){
-		       $this.log('socket error:'+ error)
-	       };
-	       this.log('websocket init');
+           };
+           this.socket.onerror = function(error){
+               $this.log('socket error:'+ error)
+           };
+           this.log('websocket init');
        },
        log: function(m) {
            console.log(m);
-           this.console.innerHTML += m + '<br />';
+           this.console.innerHTML = m + '<br />' + this.console.innerHTML;
+           contentMessage.scrollTop = 0;
        },
-       sendMessage : function(lightStatus){
-	       var message = '{"light":"' + lightStatus + '"}';
-	       this.socket.send(message);
+       sendMessage: function(lightStatus){
+           var message = '{"light":"' + lightStatus + '"}';
+           this.socket.send(message);
            this.log('message sent')
        }
 };
@@ -55,20 +56,20 @@ var socket = new WebsocketClass('ws://lwpa-dev.kermit.orange-labs.fr/_ws/demo/ws
 socket.initWebsocket();
 
 if (buttonOn.addEventListener) {
-       buttonOn.addEventListener('click',function(e){
-	       e.preventDefault();
-	       socket.sendMessage("on");
-	       return false;
+        buttonOn.addEventListener('click',function(e){
+           e.preventDefault();
+           socket.sendMessage("on");
+           return false;
        }, true);
        buttonOff.addEventListener('click',function(e){
-	       e.preventDefault();
-	       socket.sendMessage("off");
-	       return false;
+           e.preventDefault();
+           socket.sendMessage("off");
+           return false;
        }, true);
        buttonBlink.addEventListener('click',function(e){
-	       e.preventDefault();
-	       socket.sendMessage("blink");
-	       return false;
+           e.preventDefault();
+           socket.sendMessage("blink");
+           return false;
        }, true);
 } else {
        console.log('your browser does not support addevenlistener');
